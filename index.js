dba4c537651ad64c37981004d14ad53f6247f795
Enk0rt <delovoyartem4ick@gmail.com>
2024-12-06 22:08:53 +0200

    [update] - [file]: JS-module (added custom modal with errors)

--- index.js
+++ index.js
@@ -5,42 +5,44 @@ const addBtn = document.getElementById('add')
 const sortNameBtn = document.getElementById('name-sort')
 const sortValueBtn = document.getElementById('value-sort')
 const deleteBtn = document.getElementById('delete')
+let errorContainer = document.querySelector('.error__container')
 
 //Визначаємо поле для збереження пар
 const storage = document.getElementById('storage')
 
 //Робота з Local Storage, збереження роботи після перезавантаження. Отримуємо масив пар з ЛС, з кожної пари беремо перший елемент(назва, другий - значення), якщо такого масиву немає, то задаємо пустий масив і далі з ним працюємо.
-document.addEventListener("DOMContentLoaded", ()=>{
-    let lsPairElements = getFromLocaleStorage('pairs');
+
+let lsPairElements = getFromLocaleStorage('pairs') || []
     storage.innerHTML = '';
-    if(lsPairElements){
-        lsPairElements.forEach(item =>{
-            let name = item[0]
-            let value = item[1]
-            createListElement(name,value);
-        })
-    }else{
-        lsPairElements = [];
-    }
+    lsPairElements.forEach(item =>{
+        let name = item[0]
+        let value = item[1]
+        createListElement(name,value);
+    })
 
 //Створюємо елемент списку з унікальним ID, а також переносимо введене значення інпута в контент створеного елементу
     addBtn.addEventListener('click', ()=>{
-        //Отримуємо значення з інпута
+        //Отримуємо значення з інпуту
         const userInputValue = userInput.value;
-        if(!userInputValue.match(/=/g)){
-            alert('Please enter valid values!')
+        //Додаємо перевірку чи ввів користувач розділювач
+        if(userInputValue === ''){
+            createError('Please fill the input field! The template is - Name=Value')
+        }else if(!userInputValue.match(/=/g)){
+            createError('Please enter valid values! You should use "=" for splitting your Name and Value!')
         }
+
         let userInputStrArr = userInputValue.split('=');
 
-        //Розбиваємо отриману з інпута строку на два значення, перше - перед знаком "=", друге - після
+        //Розбиваємо отриману з інпуту строку на два значення, перше - перед знаком "=", друге - після
         const name = userInputStrArr[0].trim();
         const value = userInputStrArr[1].trim();
 
         //Валідація значень в інпуті
         if((!name || !value) || (name.match(/[^\w\sа-яА-ЯїЇєЄіІґҐ]/g) || (value.match(/[^\w\sа-яА-ЯїЇєЄіІґҐ]/g)))){
-            alert('Please enter valid values!')
+            //Якщо перевірка не пройдена виводимо помилку
+            createError('Please enter valid values! You can use only Alpha-numeric characters and "_" !')
         }else if(userInputValue.match(/=/g).length>1){
-            alert('Please enter only one "="!')
+            createError('Please enter only one "=" !')
         }
         else{
             //Створюємо елемент списку
@@ -63,6 +65,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         if(e.target!== storage){
             const itemId = toDeleteItem.dataset.id;
 
+            //Якщо у масиві уже є id вибраного елементу, то він видаляється з масиву методом filter й зникає клас, якщо немає, то id додається до масиву
             if (toDelete.includes(itemId)){
                 toDelete = toDelete.filter(id => id !== itemId);
                 toDeleteItem.classList.remove('chosen');
@@ -70,45 +73,38 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 toDelete.push(itemId);
                 toDeleteItem.classList.add('chosen');
             }
-            console.log(toDelete);
         }
     })
 
     //Подія при якій видаляються елементи, обрані користувачем
     deleteBtn.addEventListener('click',()=>{
+        //Для кожного елементу в масиві toDelete шукаємо відповідний елемент з data-id, якщо такий є, то видаляємо
         toDelete.forEach(id => {
             const elementToDelete = document.querySelector(`[data-id="${id}"]`)
             if(elementToDelete){
                 elementToDelete.remove();
             }
         });
+        //Після операції очищаємо масив
         toDelete = [];
 
-        //Видалення та оновлення пар у ЛС
-        let lsPairElement = getFromLocaleStorage('pairs');
-        lsPairElement = [];
-        console.log(lsPairElement)
-        Array.from(storage.children).forEach(item => {
-            let name = item.innerText.split('=')[0].trim()
-            let value = item.innerText.split('=')[1].trim()
-
-            let pair = [name,value]
-            lsPairElement.push(pair)
-        });
-        setToLocaleStorage('pairs',lsPairElement);
+        //Оновлення пар у ЛС і DOM
+        updateList()
     })
 
     //Сортуємо за Name
     sortNameBtn.addEventListener('click',()=>{
         sortArr(0);
+        //Оновлюємо контент
+        updateList()
     })
 
     //Сортуємо за Value
     sortValueBtn.addEventListener('click',()=>{
         sortArr(1);
+        //Оновлюємо контент
+        updateList()
     })
-
-})
 
 //Функція для отримання значення з ЛС
 function getFromLocaleStorage(item){
@@ -139,16 +135,63 @@ function createListElement(name,value){
 
 //Функція для сортування елементів (i - це name та value, оскільки коли формується пара - це масив з двох елементів, у такому разі name - 0, value - 1)
 function sortArr(i){
+    //Створюємо масив з дочірніх елементів списку
     let listElements = Array.from(storage.children);
+
+    //Визначаємо Name першого елементу й Name другого елементу й порівнюємо їх
     listElements.sort((a,b) => {
         const firstElementName = a.innerText.split('=')[i].trim().toLowerCase()
         const secondElementName = b.innerText.split('=')[i].trim().toLowerCase()
 
         return firstElementName.localeCompare(secondElementName);
     })
-
+    //Очищаємо список
     storage.innerHTML = '';
 
+    //Заповняємо з нуля елементами відсортованого списку
     listElements.forEach(item => storage.appendChild(item));
 }
 
+//Функція для оновлення відображення списку через Локал сторедж
+function updateList() {
+        //Отримали дані з ЛС
+    let lsPairElement = getFromLocaleStorage('pairs');
+
+        //Обнулили отриманий масив
+    lsPairElement = [];
+
+        //Формуємо масив з елементів списку й ітеруємо кожен з них
+    Array.from(storage.children).forEach(item => {
+
+        //Для кожного елементу визначаємо значення name та value отримане після розділення строки по знаку "="
+        let name = item.innerText.split('=')[0].trim()
+        let value = item.innerText.split('=')[1].trim()
+
+        //Отримані дані згруповуємо у масив
+        let pair = [name, value]
+
+        //Своєю чергою масив pair запаковуємо у загальний масив
+        lsPairElement.push(pair)
+    });
+    //Передаємо загальний масив назад у ЛС
+    setToLocaleStorage('pairs', lsPairElement);
+}
+
+function createError(text){
+        let errorElement = document.createElement("p")
+        errorContainer.appendChild(errorElement);
+        errorElement.className = ('error');
+        errorElement.innerText = `${text}`
+
+        //Асинхронно додаємо клас з анімацією зникнення через 3.4 секунди
+        setTimeout(() => {
+            errorElement.classList.add('fade-out');
+        }, 3400);
+
+        //Видаляємо елементи через 5 секунд після появи
+        setTimeout(() => {
+            if (errorElement.parentElement) { //Перевірка чи елемент існує в DOM
+                errorElement.remove();
+            }
+        }, 5000);
+}
